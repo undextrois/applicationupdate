cade8c34429d2daf1ce61771040989004b43dcad
undextrois <undextrois@gmail.com>
2025-10-29 23:04:10 -0400

    Refactor ApplicationUpdate for improved download handling

--- ApplicationUpdate.java
+++ ApplicationUpdate.java
@@ -1,149 +1,123 @@
-package com.[YOUR-DOMAIN-NAME].android;
-
-import com.[YOUR-DOMAIN-NAME].android.DownloadManager;
-import java.io.InputStream;
-import java.security.Signature;
+package com.yourdomain.android;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
-//foo: added
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-
-import org.apache.http.util.ByteArrayBuffer;
-
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.os.Message;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-//foo: added-end
-import org.apache.http.util.ByteArrayBuffer;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import android.util.Log;
+import java.io.File;
 
-public class ApplicationUpdate extends Activity implements OnClickListener {
-    ProgressDialog dialog;
-    int increment;
-    int val;
-   
-    private final static String PATH = "/data/data/com.[YOUR-DOMAIN-NAME].android/";
+/**
+ * ApplicationUpdate Activity
+ * Handles downloading and updating the app from a specified URL.
+ */
+public class ApplicationUpdate extends Activity implements View.OnClickListener {
+
+    private static final String TAG = "ApplicationUpdate";
+    private static final String DOWNLOAD_URL = "http://www.webdunnit.com/";
+    private static final String FILE_NAME = "home_page.jpg";
+    private static final String APP_PATH = "/data/data/com.yourdomain.android/";
+
+    private ProgressBar progressBar;
+    private Handler uiHandler;
+    private boolean isDownloading = false;
+
     @Override
-    
-    public void onCreate(Bundle savedInstanceState) 
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.application_update);
-        Button startbtn = (Button) findViewById(R.id.startbtn);
-        startbtn.setOnClickListener(this);
-        
-        Log.i("sys","system property:"+System.getProperties() );
-        Log.i("","STORAGE DIRECTORY: "+ Environment.getExternalStorageDirectory() );
-        Log.i("CREATE","initialize everything.....");
+
+        progressBar = findViewById(R.id.progressBar); // Replace with actual ProgressBar in layout
+        Button startButton = findViewById(R.id.startbtn);
+        startButton.setOnClickListener(this);
+
+        uiHandler = new Handler(Looper.getMainLooper());
+
+        Log.i(TAG, "System properties: " + System.getProperties());
+        Log.i(TAG, "Storage directory: " + Environment.getExternalStorageDirectory());
+        Log.i(TAG, "Initialization complete.");
     }
-    
-    public void onClick(View view) { 
-    	dialog = new ProgressDialog(this);
-        dialog.setCancelable(true);
-        dialog.setMessage("Downloading Updates...");
-        Log.i("ONLCLICK","start downloading...");
-        // set the progress to be horizontal
-        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        // reset the bar to the default value of 0
-        dialog.setProgress(0);
- 
-        DownloadManager.DownloadFromUrl("http://www.webdunnit.com/", "home_page.jpg");
-        val = DownloadManager.M_statusProgress;
-        
-        dialog.setMax(val);
-        Log.i("ONLCLICK","update the progress bar..");
-        //end test
-        //dialog.setMax(maximum);
-        // display the progressbar
-        dialog.show();
- 
-        // create a thread for updating the progress bar
-        Thread background = new Thread (new Runnable() {
-           public void run() {
-               try {
-                   // enter the code to be run while displaying the progressbar.
-                   //
-                   //  increment the progress bar:
-                   // So keep running until the progress value reaches maximum value
-            	  // ImageManager.DownloadFromUrl("http://www.webdunnit.com/", "bhome.jpg");
-            	   while(dialog.getProgress() <= DownloadManager.M_statusProgress) {
-            	//   while (dialog.getProgress() <= dialog.getMax()) {
-                       // wait 500ms between each update
-                       Thread.sleep(500);
- 
-                       // active the update handler
-                       progressHandler.sendMessage(progressHandler.obtainMessage());
-                   }
-               } catch (java.lang.InterruptedException e) {
-                   // if something fails do something smart
-            	   Log.i("THREAD:","Something wrong has happened....");
-               }
-           }
-        });
- 
-        // start the background thread
-        background.start();
- 
-    }
- 
-    // handler for the background updating
-    Handler progressHandler = new Handler() {
-        public void handleMessage(Message msg) {
-          dialog.incrementProgressBy(val);
-            
-            if(dialog.getProgress() == dialog.getMax())
-            {
-            	dialog.dismiss();
-            //	alertbox("Install Update","This will install updates on your device.");
-            }
-            /*
-            if(dialog.getProgress() >= 100) {
-              dialog.dismiss();
-            }else {
-              dialog.incrementProgressBy(increment);
-            }*/
- 
+
+    @Override
+    public void onClick(View view) {
+        if (isDownloading) {
+            Toast.makeText(this, "Download already in progress", Toast.LENGTH_SHORT).show();
+            return;
         }
-    };
-	protected void alertbox(String title, String mymessage)
-	{
-	   try {
-		new AlertDialog.Builder(this)
-	      .setMessage(mymessage)
-	      .setTitle(title)
-	      .setCancelable(true)
-	      .setNeutralButton("OK",
-	         new DialogInterface.OnClickListener() {
-	         public void onClick(DialogInterface dialog, int whichButton){
-	        	 String fileName = "/data/data/com.[YOUR-DOMAIN-NAME].android/xmas.jpg";
-	        	 
-	        	 Intent intent = new Intent(Intent.ACTION_VIEW);
-	        	 intent.setDataAndType(Uri.fromFile(new File(fileName)), "application/vnd.android.package-archive");
-	        	 startActivity(intent);
-	         }
-	         })
-	      .show();
-	   }catch(Exception e ){
-		   e.printStackTrace();
-	   }
-	 }
+
+        isDownloading = true;
+        Toast.makeText(this, "Starting update download...", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "Starting download from " + DOWNLOAD_URL);
+
+        // Run the download in a background thread
+        new Thread(() -> {
+            try {
+                // Perform download
+                DownloadManager.DownloadFromUrl(DOWNLOAD_URL, FILE_NAME);
+
+                // Update progress bar continuously
+                while (DownloadManager.M_statusProgress < 100) {
+                    int progress = DownloadManager.M_statusProgress;
+                    uiHandler.post(() -> progressBar.setProgress(progress));
+                    Thread.sleep(500);
+                }
+
+                // Once complete
+                uiHandler.post(() -> {
+                    progressBar.setProgress(100);
+                    isDownloading = false;
+                    showInstallDialog();
+                });
+
+            } catch (Exception e) {
+                Log.e(TAG, "Download failed", e);
+                uiHandler.post(() -> {
+                    isDownloading = false;
+                    Toast.makeText(this, "Download failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                });
+            }
+        }).start();
+    }
+
+    /**
+     * Prompts the user to install the downloaded update.
+     */
+    private void showInstallDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Install Update")
+                .setMessage("Do you want to install the downloaded update?")
+                .setCancelable(true)
+                .setPositiveButton("Install", (dialog, which) -> installUpdate())
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    /**
+     * Launches the Android package installer for the downloaded file.
+     */
+    private void installUpdate() {
+        try {
+            File file = new File(APP_PATH, FILE_NAME);
+            Uri fileUri = Uri.fromFile(file);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(fileUri, "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to launch installer", e);
+            Toast.makeText(this, "Failed to install update", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
